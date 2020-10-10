fcbf7140e1d0c275584d93d1195268daca2ca647
赵宏磊 <72618021+Xiaolei0412@users.noreply.github.com>
2020-10-10 08:20:31 +0000

    变量的输入
    变量的输入

---
+++ 变量的输入.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	double b;
+	char c;
+	cin >> a >> b >> c;
+	cout << "a=" << a << "\nb=" <<b<< "\nc=" << c << "\n";
+	            //  \n 的意思是：回车换行。将当前位置移到下一行的开头。
+		system("pause");
+	return 0;
+}
