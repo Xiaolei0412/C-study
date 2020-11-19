960c611df9042d2b68dd804d9c8a0a95ab3a7541
赵宏磊 <72618021+Xiaolei0412@users.noreply.github.com>
2020-11-19 21:33:12 +0800

    【函数-定义-调用-内联函数】
    函数-定义-调用-内联函数

---
+++ 第一阶段基础：函数/函数-定义-调用-内联函数.cpp
@@ -0,0 +1,43 @@
+/*
+	函数的定义：1.无参函数 <type> <函数名>() {}
+					void ptint_title()
+						{ cout << "00" << endl; }
+				2.有参函数 
+					int max(int x,int y) {return ( x>y ? x:y );}
+				3. 形式参数（函数名括号中所列举的参数）
+					float f(float x,float y,int m)
+						{ ... }  //函数体
+	函数的调用：
+	函数常见样式：
+		
+*/
+
+
+#include<iostream>
+using namespace std;
+
+//函数的调用，输入两个实数，求其中最大的数
+//intline 定义内联函数：占用更多的存储空间而减少执行时间。
+inline float max(float x, float y)
+{
+	return (x > y ? x : y);   //被调函数必须在前面才能正常调用
+}
+
+int main()
+{
+	//float max(float x, float y);  //如果没有个就运行不了
+	float a, b;
+	cout << "输入两个实数" << endl;
+
+	cin >> a >> b;
+	cout << "最大的数为：" << max(a, b) << endl;
+	system("pause");
+	return 0;  
+}
+/*
+float max(float x, float y)
+{
+	return (x > y ? x : y);
+}
+*/
+
